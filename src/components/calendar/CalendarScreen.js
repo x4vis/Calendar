72497c0da2e0ec838eb9b9940a9a565594b76c88
Javier Soto <javislop@gmail.com>
2021-03-16 00:05:46 -0600

    add fontawesome and nvbar progress

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../ui/Navbar'
 
 const CalendarScreen = () => {
   return (
-    <div>
-      <h1>Calendar Screen</h1>
-    </div>
+    <>
+      <Navbar /> 
+    </>
   )
 }
 
