9e23c5fa4ca1c628b910ce098b484fde1a7614af
Javier Soto <javislop@gmail.com>
2021-03-16 18:04:36 -0600

    add CalendarEvent component and create event calendar functions

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../ui/Navbar';
+import CalendarEvent from './CalendarEvent';
+import { messages } from '../../helpers/calendar-messages-es';
 
 import moment from 'moment';
-import 'moment/locale/es'
+import 'moment/locale/es';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-import { messages } from '../../helpers/calendar-messages-es';
 
 moment.locale('es');
 const localizer = momentLocalizer(moment);
@@ -16,9 +16,42 @@ const events = [{
   start: moment().toDate(),
   end: moment().add(2, 'hours').toDate(),
   bgcolor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Javier',
+  }
 }]
 
 const CalendarScreen = () => {
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+  const onDoubleClick = (e) => {
+
+  }
+
+  const onSelectEvent = (e) => {
+    
+  }
+
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem('lastView', e);
+  }
+
+  const eventStyleGetter = (event, start, end, selected) => {
+    const styles = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white',
+    }
+    
+    return {
+      styles
+    }
+  }
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -29,6 +62,14 @@ const CalendarScreen = () => {
         startAccessor="start"
         endAccessor="end"
         messages={messages}
+        eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view={lastView}
+        components={{
+          event: CalendarEvent
+        }}
       />
     </div>
   )
